44393ea2b6334beb326c68b413223961aba51b12
ZAINAL1221 <107286848+ZAINAL1221@users.noreply.github.com>
2023-05-20 05:00:49 +0800

    Add files via upload

--- plugins/_events/system/chatAI.js
+++ plugins/_events/system/chatAI.js
@@ -20,7 +20,7 @@ exports.run = {
             'who are you'
          ]
          if (m.isGroup) {
-            if (body && hint.includes(body.toLowerCase())) return m.reply('Hi, i\'am Neoxr Bot\nDo you want to chat with me? send *bot* to create a chat session.')
+            if (body && hint.includes(body.toLowerCase())) return m.reply('Hi, i\'am Khoiyrul Botz\nDo you want to chat with me? send *bot* to create a chat session.')
             for (let jid of [...new Set([...(m.mentionedJid || [])])]) {
                if (jid != client.decodeJid(client.user.id)) continue
                if (!m.fromMe) return m.reply('Do you want to chat with me? send *bot* to create a chat session.')
@@ -75,7 +75,7 @@ exports.run = {
             }
          } else {
             if (!setting.chatbot || body && body.match(/\d{3}-\d{3}/) || !/conversation|extended/.test(m.mtype)) return
-            if (body && hint.includes(body.toLowerCase())) return m.reply('Hi, i\'am Neoxr Bot')
+            if (body && hint.includes(body.toLowerCase())) return m.reply('Hi, i\'am Khoiyrul Botz')
             global.db.gpt = global.db.gpt ? global.db.gpt : {}
             if (!global.db.gpt[m.sender]) global.db.gpt[m.sender] = []
             if (global.db.gpt[m.sender].length >= 7) global.db.gpt[m.sender] = []
